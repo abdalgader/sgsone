2fa0bd0d24eb636ebb8cad1d9f92e391810e936b
Dominic Reich <dom.reich@gmail.com>
2011-12-26 12:55:29 +0100

    updated readme files and some basics on the signapk batch scripts

--- SignApk-Batchscript/batch-linux.sh
+++ SignApk-Batchscript/batch-linux.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+# Author: Dominic Reich <www.dark-fellow.info>
+# 2011/2010
+#
+# creates a *SIGNED* 'update.zip' file from an *UNSIGNED* 'myupdate.zip' file
+# you still have to install SignApk Application yourself and
+# you still have to generate certificates for this.
+#
+# and of course: YOU NEED JAVA JRE FOR THIS TO WORK!
+
 java -jar signapk.jar certificate.pem key.pk8 myupdate.zip update.zip
 rm myupdate.zip
--- SignApk-Batchscript/batch.bat
+++ SignApk-Batchscript/batch.bat
@@ -1,4 +1,12 @@
 @echo off
+@REM Author: Dominic Reich <www.dark-fellow.info>
+@REM 2011/2010
+@REM
+@REM creates a *SIGNED* 'update.zip' file from an *UNSIGNED* 'myupdate.zip' file
+@REM you still have to install SignApk Application yourself and
+@REM you still have to generate certificates for this.
+
+@REM and of course: YOU NEED JAVA JRE FOR THIS TO WORK!
 
 java -jar signapk.jar certificate.pem key.pk8 myupdate.zip update.zip
 del myupdate.zip
